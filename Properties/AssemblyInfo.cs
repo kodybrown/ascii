b2903510cbfaa6215292968a181324e60464fc27
Kody Brown <kody@bricksoft.com>
2013-07-06 01:36:55 -0600

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ascii")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Displays the ascii characters from command-line")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ascii")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright (C) 2013 Kody Brown.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
